b8f449dece4c041d423dffa300a3add9c75ce8ca
aeroninou <aeroninouye@live.com>
2023-01-03 22:54:08 +0000

    learning about input functions

---
+++ lab_input/movie2.py
@@ -0,0 +1,22 @@
+
+
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE 01 - Solution"""
+
+def main():
+
+    user_input = input("Please enter a movie: ")
+
+    ## the line below creates a single string that is passed to print()
+    # print("You told me the IPv4 address is:" + user_input)
+
+    ## print() can be given a series of objects separated by a comma
+    print("You told me a movie called:", user_input)
+
+    # asking user for 'vendor name'
+    snack = input("Please enter favorite movie snack: ")
+    print(snack)
+
+main()
+
