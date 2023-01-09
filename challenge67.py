7a1679ffac20a45df11e2ed773425440f3228a37
aeroninou <aeroninouye@live.com>
2023-01-09 04:26:00 +0000

    vampire challenge 67

---
+++ challenge67.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""aeroninou | aeron.inouye@tlgcohort.com"""
+
+def main():
+    """runtime function"""
+
+    count = 0
+    with open ("dracula.txt", "r") as dracula:
+        with open("vampytimes.txt", "w") as new_dracula:
+            for line in dracula:
+                if "vampire" in line.lower():
+                    count += 1
+                    new_dracula.write(line)
+    print(count)
+
+# call our main function
+if __name__ == "__main__":
+    main()
